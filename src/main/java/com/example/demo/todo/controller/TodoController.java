5886dc79d8a4e0dae98211985a4bd9b66401fca1
prudhvipotnuru <prudhvipotnuru@gmail.com>
2025-12-28 07:36:16 +0530

    used lombok for getters and setters

--- src/main/java/com/example/demo/todo/controller/TodoController.java
+++ src/main/java/com/example/demo/todo/controller/TodoController.java
@@ -1,6 +1,5 @@
 package com.example.demo.todo.controller;
 
-import java.util.Collections;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.todo.model.Todo;
